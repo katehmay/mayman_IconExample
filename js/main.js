6896d69f186c6a9240964f1191bb2a147b6a0870
Mayman <k_mayman@tss-hr420-dm08.local>
2019-05-21 16:00:22 -0400

    playing with javascript variables

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
 
 	// select elements with JavaScript
 	// and using a CSS selector to find the element we want to use
-	// 
+	//
 	// let stores the reference to element as a variable (in memory)
-	let svgGraphic = document.querySelector("#badgeSVG");
+	let svgGraphic = document.querySelector("#badgeSVG"),
+			mainHeadline = document.querySelector(".main-headline"),
+			rasterVector = document.querySelector(".raster-vector-text");
 
 	//functions are reusable pieces of code
 	//that you can run any time
@@ -18,4 +20,6 @@
 
 	svgGraphic.addEventListener("click", logMyId);
 
+	mainHeadline.textContent = "Now you're something else!";
+	rasterVector.textContent = "I'm changing things very sneakily in the javascript file";
 })();
